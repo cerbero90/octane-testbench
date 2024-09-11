0d637beade76e9549642b746131987bbf35961e2
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2024-09-11 16:36:22 -0300

    Add Octane service provider

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Cerbero\OctaneTestbench;
 
+use Laravel\Octane\OctaneServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 /**
@@ -43,5 +44,18 @@ abstract class TestCase extends OrchestraTestCase
     protected function getOctaneBinPath(): string
     {
         return __DIR__ . '/bin';
+    }
+
+    /**
+     * Get package providers.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     * @return array<int, class-string<\Illuminate\Support\ServiceProvider>>
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            OctaneServiceProvider::class,
+        ];
     }
 }
